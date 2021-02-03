8387741c7f58280b3991974ca0450608ba25766f
hardcomand <35082983+hardcomand@users.noreply.github.com>
2021-02-03 16:28:35 -0300

    Update TelaRelogio.java
    Primeiro pequeno projeto.

--- TelaRelogio.java
+++ TelaRelogio.java
@@ -1,8 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+*New project, starting to learn java
+*/
 package cursoemvideo;
 
 import java.util.Date;
